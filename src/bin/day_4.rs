8bfcbe7676a39d36caf8264a1581bfdf86e72c26
Anthony Mason <anthony81799@gmail.com>
2022-12-04 15:43:18 -0500

    day 4

---
+++ src/bin/day_4.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let vec_map = |t: Vec<&str>| t.iter().map(|s| s.parse::<u8>().unwrap()).collect();
+    let input: Vec<Vec<u8>> = include_str!("../inputs/day_4.txt")
+        .lines()
+        .map(|l| l.split(','))
+        .map(|mut s| (s.next().unwrap().split('-'), s.next().unwrap().split('-')))
+        .map(|(e1, e2)| vec_map(e1.chain(e2).collect()))
+        .collect();
+
+    let p1 = input
+        .iter()
+        .filter(|nums| {
+            (nums[0] >= nums[2] && nums[1] <= nums[3]) || (nums[2] >= nums[0] && nums[3] <= nums[1])
+        })
+        .count();
+    let p2 = input
+        .iter()
+        .filter(|nums| nums[0] <= nums[3] && nums[2] <= nums[1])
+        .count();
+    println!("{p1} {p2}");
+}
